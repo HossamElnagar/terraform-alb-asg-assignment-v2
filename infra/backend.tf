e17b6344e742425ef822d331cad543725ec8a4f4
HossamElnagar <Hossam@gmail.com>
2026-06-21 15:58:48 +0300

    7

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,7 +4,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "digilians-tfstate"
+    bucket         = "digilians-tfstate-us-west-1"
     key            = "assignment/alb-asg/terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "digilians-tfstate-lock"
